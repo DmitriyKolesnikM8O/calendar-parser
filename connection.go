b06b771bc65e2834d5970561c52fb7d6d6dda182
Dmitriy <kolesnik982011@gmail.com>
2025-07-07 15:50:34 +0300

    get statistics function out of main

--- connection.go
+++ connection.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -43,8 +42,6 @@ var (
 	}
 
 	timeStart, timeEnd string
-
-	mode = flag.Bool("gui", false, "Running in GUI mode")
 )
 
 func getToken(config *oauth2.Config) (*oauth2.Token, error) {
